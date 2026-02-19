0eec362e113d97f434e181c3c4ecb4427db6a1b0
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-02-19 10:47:08 +0530

    Payment Model Added

---
+++ GymManagementSystem/Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿namespace GymManagementSystem.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime PaymentDate { get; set; } = DateTime.Now;
+
+        public string PaymentMethod { get; set; }
+    }
+}
