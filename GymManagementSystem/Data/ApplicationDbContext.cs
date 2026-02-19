205cc4bbdecd10077f75646c25db069bf07c9840
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-02-19 13:59:22 +0530

    Configure the application db  contect

---
+++ GymManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using GymManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace GymManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<MembershipPlan> MembershipPlans { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // One Trainer → Many Members
+            modelBuilder.Entity<Member>()
+                .HasOne(m => m.Trainer)
+                .WithMany(t => t.Members)
+                .HasForeignKey(m => m.TrainerId);
+
+            // One Plan → Many Members
+            modelBuilder.Entity<Member>()
+                .HasOne(m => m.MembershipPlan)
+                .WithMany(p => p.Members)
+                .HasForeignKey(m => m.MembershipPlanId);
+        }
+    }
+}
