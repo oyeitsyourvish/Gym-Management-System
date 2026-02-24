44d40574760e9563148659a335455069d58a46a8
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-02-25 01:10:58 +0530

    solve internal issue

--- GymManagementSystem/obj/Debug/net8.0/GymManagementSystem.AssemblyInfo.cs
+++ GymManagementSystem/obj/Debug/net8.0/GymManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317028b862a527ca054fc1839e495efb941c8fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3961f1dc146b3ce90df8ae1847f1b229b0422a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
