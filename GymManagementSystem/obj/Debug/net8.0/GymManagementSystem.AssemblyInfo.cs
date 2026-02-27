baab148315484d02e10e3f60b4b4552c2cc620fc
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-02-28 00:37:08 +0530

    Modified Ui for Member Edit, Delete And Creat view

--- GymManagementSystem/obj/Debug/net8.0/GymManagementSystem.AssemblyInfo.cs
+++ GymManagementSystem/obj/Debug/net8.0/GymManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09879d04512fbffed6665137b07c4c8ed6308db2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6749e199a3be591f754cd5c0143ce5bc82369a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
